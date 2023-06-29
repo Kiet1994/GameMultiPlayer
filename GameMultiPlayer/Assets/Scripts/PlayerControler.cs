72b7a821a8d12cf7a769873506f2c5fa1209f535
HOANGKIET\DangTran <phanhoangkiet1994@gmail.com>
2023-06-29 18:37:52 +0700

    Add netcode and move player.

---
+++ GameMultiPlayer/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using Unity.VisualScripting;
+
+public class PlayerControler : NetworkBehaviour
+{
+    [SerializeField] private float speed = 3f   ;
+    private Camera _mainCamera;
+    private Vector3 _mouseInput = Vector3.zero;
+
+    private void Initialize() // Khởi tạo
+    {
+        _mainCamera = Camera.main;
+    }
+    public override void OnNetworkSpawn()// sử dụng giống awake
+    {
+        base.OnNetworkSpawn();
+        Initialize();
+    }
+
+    void Update()
+    {
+        if (!IsOwner || !Application.isFocused) return; // nếu không phải chủ của scprit chứa gameobject và ở ngoài vùng game sẽ không hoạt động
+        //movement
+        _mouseInput.x = Input.mousePosition.x;
+        _mouseInput.y = Input.mousePosition.y;
+        _mouseInput.z = _mainCamera.nearClipPlane;
+        Vector3 mouseWorldCoordinates = _mainCamera.ScreenToWorldPoint(_mouseInput); //toạ độ thế giới
+        transform.position = Vector3.MoveTowards(transform.position, mouseWorldCoordinates, Time.deltaTime * speed);
+        //rotate
+         if (mouseWorldCoordinates != transform.position)
+        {
+            Vector3 targetDirection = mouseWorldCoordinates - transform.position;
+            targetDirection.z = 0;
+            transform.up = targetDirection;
+        }
+    }
+}
